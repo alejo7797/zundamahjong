7355d144806582bebb0e9c7203a653d9af408546
faraplay <faraplay@outlook.com>
2026-08-05 16:20:38 +0100

    add shanten scores for other actions

--- src/zundamahjong/mahjong/bot.py
+++ src/zundamahjong/mahjong/bot.py
@@ -1,8 +1,10 @@
+from collections.abc import Sequence
+
 from .action import Action, ActionType
 from .call import get_call_tiles
 from .info import AllGameInfo
 from .shanten import calculate_shanten
-from .tile import TileValue, all_tiles, get_tile_value, tile_id_is_flower
+from .tile import TileId, TileValue, all_tiles, get_tile_value, tile_id_is_flower
 
 
 def calculate_bot_action(info: AllGameInfo) -> Action:
@@ -31,7 +33,7 @@ def calculate_bot_action(info: AllGameInfo) -> Action:
     if all(not tile_id_is_flower(tile) for tile in info.player_info.hand):
         freqs = unseen_frequencies(info)
         for action in action_scores:
-            action_scores[action] += get_shanten_score(action, info, freqs)
+            action_scores[action] += get_action_shanten_score(action, info, freqs)
 
     best_action, _ = max(action_scores.items(), key=lambda a: a[1])
     return best_action
@@ -57,21 +59,53 @@ def unseen_frequencies(info: AllGameInfo) -> dict[TileValue, int]:
     return freqs
 
 
-def get_shanten_score(
+def get_action_shanten_score(
     action: Action, info: AllGameInfo, freqs: dict[TileValue, int]
 ) -> int:
     if (
         action.action_type == ActionType.DISCARD
         or action.action_type == ActionType.RIICHI
+        or action.action_type == ActionType.ADD_KAN
     ):
-        hand_values = [
-            get_tile_value(tile)
-            for tile in info.player_info.hand
-            if tile != action.tile
-        ]
-        (shanten, useful_tiles) = calculate_shanten(
-            hand_values, is_3player=(info.player_count == 3)
+        return remove_tile_get_shanten_score(
+            action.tile,
+            info.player_info.hand,
+            info,
+            freqs,
         )
-        useful_tiles_count = sum(freqs[tile_value] for tile_value in useful_tiles)
-        return 1000 - 100 * shanten + useful_tiles_count
+    if action.action_type == ActionType.CLOSED_KAN:
+        new_hand = [t for t in info.player_info.hand if t not in action.tiles]
+        return get_shanten_score(new_hand, info, freqs)
+    if action.action_type == ActionType.PASS or action.action_type == ActionType.DRAW:
+        return get_shanten_score(info.player_info.hand, info, freqs)
+    if action.action_type == ActionType.CHII or action.action_type == ActionType.PON:
+        new_hand = [t for t in info.player_info.hand if t not in action.other_tiles]
+        return get_best_shanten_score(new_hand, info, freqs)
+    if action.action_type == ActionType.OPEN_KAN:
+        new_hand = [t for t in info.player_info.hand if t not in action.other_tiles]
+        return get_shanten_score(new_hand, info, freqs)
+
     return 0
+
+
+def get_shanten_score(
+    hand: Sequence[TileId], info: AllGameInfo, freqs: dict[TileValue, int]
+) -> int:
+    (shanten, useful_tiles) = calculate_shanten(
+        [get_tile_value(tile) for tile in hand], is_3player=(info.player_count == 3)
+    )
+    useful_tiles_count = sum(freqs[tile_value] for tile_value in useful_tiles)
+    return -100 * shanten + useful_tiles_count
+
+
+def remove_tile_get_shanten_score(
+    tile: TileId, hand: Sequence[TileId], info: AllGameInfo, freqs: dict[TileValue, int]
+) -> int:
+    new_hand = [t for t in hand if t != tile]
+    return get_shanten_score(new_hand, info, freqs)
+
+
+def get_best_shanten_score(
+    hand: Sequence[TileId], info: AllGameInfo, freqs: dict[TileValue, int]
+) -> int:
+    return max(remove_tile_get_shanten_score(tile, hand, info, freqs) for tile in hand)
