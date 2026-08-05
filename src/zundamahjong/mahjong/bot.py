8dc42075939e2989fb0af5df5a9a7c9417cf5f9b
faraplay <faraplay@outlook.com>
2026-08-05 13:25:19 +0100

    add function to calculate freqs of unseen tiles

--- src/zundamahjong/mahjong/bot.py
+++ src/zundamahjong/mahjong/bot.py
@@ -1,7 +1,9 @@
 from time import sleep
 
 from .action import Action
-from .info import AllGameInfo
+from .call import get_call_tiles
+from .info import AllGameInfo, RoundInfo
+from .tile import TileValue, all_tiles, get_tile_value
 
 
 def calculate_bot_action(info: AllGameInfo) -> Action:
@@ -12,6 +14,28 @@ def calculate_bot_action(info: AllGameInfo) -> Action:
     :param info: An object with the info of the game state from the bot's point of view.
     """
 
-    if len(info.player_info.actions) > 1:
-        sleep(0.5)
+    if len(info.player_info.actions) == 1:
+        return info.player_info.actions[0]
+
+    sleep(0.5)
     return info.player_info.actions[0]
+
+
+def unseen_frequencies(info: AllGameInfo) -> dict[TileValue, int]:
+    freqs = {tile_value: 4 for tile_value in all_tiles}
+
+    for tile in info.player_info.hand:
+        freqs[get_tile_value(tile)] -= 1
+    for player_calls in info.round_info.calls:
+        for call in player_calls:
+            for tile in get_call_tiles(call):
+                freqs[get_tile_value(tile)] -= 1
+    for discard in info.round_info.discards:
+        if (
+            not discard.is_called
+            and not discard.is_added_kan
+            and not discard.is_closed_kan
+        ):
+            freqs[get_tile_value(discard.tile)] -= 1
+
+    return freqs
